8b2d44293cdf28b650abf5f6e629dc17f1ad1806
George Livas <giorgos.livas.99@gmail.com>
2017-01-05 21:38:05 +0200

    init buttons

--- tik-tak-toe/ViewController.swift
+++ tik-tak-toe/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// 1 == cross / 2 == nought
+    var activePlayer = 1
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
+    
+    @IBAction func action(_ sender: Any) {
+        
+        if (gameState[(sender as AnyObject).tag-1] == 0){
+            if (activePlayer == 1) {
+                (sender as AnyObject).setImage(UIImage(named: "Cross.png"), for: UIControlState())
+                activePlayer = 2
+            } else {
+                (sender as AnyObject).setImage(UIImage(named: "Nought.png"), for: UIControlState())
+                activePlayer = 1
+                
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
