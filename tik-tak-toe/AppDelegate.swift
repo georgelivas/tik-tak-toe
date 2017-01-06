5e1ee7c364de9830222ace8a83ff1416c0af46f7
George Livas <giorgos.livas.99@gmail.com>
2017-01-06 13:59:31 +0200

    up and running again!

--- tik-tak-toe/AppDelegate.swift
+++ tik-tak-toe/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  tik-tak-toe
+//  Tic Tac Toe
 //
-//  Created by Giorgos livas on 05/01/2017.
-//  Copyright © 2017 Giorgos livas. All rights reserved.
+//  Created by Sebastian Hette on 10.10.2016.
+//  Copyright © 2016 MAGNUMIUM. All rights reserved.
 //
 
 import UIKit
@@ -53,7 +53,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "tik_tak_toe")
+        let container = NSPersistentContainer(name: "Tic_Tac_Toe")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
--- tik-tak-toe/ViewController.swift
+++ tik-tak-toe/ViewController.swift
@@ -1,49 +1,99 @@
 //
 //  ViewController.swift
-//  tik-tak-toe
+//  Tic Tac Toe
 //
-//  Created by Giorgos livas on 05/01/2017.
-//  Copyright © 2017 Giorgos livas. All rights reserved.
+//  Created by Sebastian Hette on 10.10.2016.
+//  Copyright © 2016 MAGNUMIUM. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-// 1 == cross / 2 == nought
-    var activePlayer = 1
-    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
     
-    let winnerIf = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
+    var activePlayer = 1 //Cross
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
-    var gameRunning = true
-    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    var gameIsActive = true
     
     @IBOutlet weak var label: UILabel!
     
-   
-    @IBAction func action(_ sender: Any) {
-        
-        if (gameState[(sender as AnyObject).tag-1] == 0){
-            if (activePlayer == 1) {
-                (sender as AnyObject).setImage(UIImage(named: "Cross.png"), for: UIControlState())
+    @IBAction func action(_ sender: AnyObject)
+    {
+        if (gameState[sender.tag-1] == 0 && gameIsActive == true)
+        {
+            gameState[sender.tag-1] = activePlayer
+            
+            if (activePlayer == 1)
+            {
+                sender.setImage(UIImage(named: "Cross.png"), for: UIControlState())
                 activePlayer = 2
-            } else {
-                (sender as AnyObject).setImage(UIImage(named: "Nought.png"), for: UIControlState())
-                activePlayer = 1
-                
             }
+            else
+            {
+                sender.setImage(UIImage(named: "Nought.png"), for: UIControlState())
+                activePlayer = 1
+            }
+        }
+        
+        for combination in winningCombinations
+        {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
+            {
+                gameIsActive = false
+                
+                if gameState[combination[0]] == 1
+                {
+                    label.text = "CROSS HAS WON!"
+                }
+                else
+                {
+                    label.text = "NOUGHT HAS WON!"
+                }
+                
+                playAgainButton.isHidden = false
+                label.isHidden = false
+            }
+        }
+        
+        gameIsActive = false
+        
+        for i in gameState
+        {
+            if i == 0
+            {
+                gameIsActive = true
+                break
+            }
+        }
+        
+        if gameIsActive == false
+        {
+            label.text = "IT WAS A DRAW"
+            label.isHidden = false
+            playAgainButton.isHidden = false
         }
     }
     
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBAction func playAgain(_ sender: AnyObject)
+    {
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        gameIsActive = true
+        activePlayer = 1
+        
+        playAgainButton.isHidden = true
+        label.isHidden = true
+        
+        for i in 1...9
+        {
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: UIControlState())
+        }
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
